03ca5d377444087e409859701ae75cf3eafd9507
jerome-Aulagne <jerome.aulagne@diiage.org>
2016-11-24 14:22:41 +0100

    Delete ListprocPE.sh

--- ListprocPE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-# Affichage de la liste des processus sous forme d'arborescence , en ajoutant les liens de parentés Parent/Enfant , puis en spécifiant via les options "p" l'id de processus et "u" l'utilisateur ayant lancé le proc.
-
-pstree -up | less
