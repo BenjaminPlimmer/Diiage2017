0f5f48297517f205eaaa0205ab189f3c28401d98
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2016-11-24 14:17:17 +0100

    Add files via upload

---
+++ ShowtreeProcessPLIMMER.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Show all process and their children
+
+
+if command -v pstreee >/dev/null
+then
+        pstree -p
+else
+        echo "I require pstree but it's not installed."
+        echo "Type y to install"
+        read answer
+
+        if echo "$answer" | grep -iq "^y"
+        then
+                apt-get install psmisc >/dev/null
+                pstree -p
+        else
+                ps -faux |less
+        fi
+fi
