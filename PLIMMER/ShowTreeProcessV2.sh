f7f1cdb18c390656ac0c9866bd8bdda4d4fbde0c
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2016-11-28 11:50:59 +0100

    New script to show all process from /proc/

---
+++ PLIMMER/ShowTreeProcessV2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#@AIM : Show all process and their children
+#@AUTHORS : Benjamin PLIMMER
+#@PARAMS : None
+
+
+FILE=$(ls /proc/ | grep "[0-9]")
+
+# Save pid and ppids in file /tmp/info
+for f in $FILE; do
+	NAME=$(more /proc/$i/status 2>/dev/null | grep "Name" | awk -F " " {'print $2'}) 
+	IDP=$(more /proc/$i/status 2>/dev/null | grep "PPid" |awk -F " " {'print $2'}) 
+	echo "$f:$IDP" >> /tmp/info
+done
+
+currentlevel=0
+root='1'
+
+#AIM : Show tree from file (parent;enfant) || Stolen from internet
+#PARAMS : [INT] PID racine
+function subtree
+{
+        while IFS=":" read enfant parent
+        do
+                if [ "$parent" == "$1" ] 
+                then
+                        for (( i=0; i<$currentlevel; i++ ))
+                        do
+                                echo -e -n "|      "
+                        done
+                        echo -e "└------$enfant"
+                        currentlevel=$((currentlevel+1))
+                        subtree $enfant
+                        currentlevel=$((currentlevel-1))
+                fi
+        done < /tmp/list.csv
+
+}
+
+subtree 1
+
+rm /tmp/info
