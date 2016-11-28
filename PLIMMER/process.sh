2e6a87d9db8b1ecd0aef9c3b8ca2fb143dc7af7d
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2016-11-28 14:07:04 +0100

    Update syntax

--- PLIMMER/process.sh
+++ PLIMMER/process.sh
@@ -3,38 +3,35 @@
 #@AUTHORS : Benjamin PLIMMER
 #@PARAMS : None
 
-
 FILE=$(ls /proc/ | grep "[0-9]")
 
 # Save pid and ppids in file /tmp/info
 for f in $FILE; do
-	NAME=$(more /proc/$i/status 2>/dev/null | grep "Name" | awk -F " " {'print $2'}) 
-	IDP=$(more /proc/$i/status 2>/dev/null | grep "PPid" |awk -F " " {'print $2'}) 
-	echo "$f:$IDP" >> /tmp/info
+   NAME=$(more /proc/$i/status 2>/dev/null | grep "Name" | awk -F " " {'print $2'}) 
+   IDP=$(more /proc/$i/status 2>/dev/null | grep "PPid" |awk -F " " {'print $2'}) 
+   echo "$f:$IDP" >> /tmp/info
 done
 
 currentlevel=0
-root='1'
 
 #AIM : Show tree from file (parent;enfant) || Stolen from internet
 #PARAMS : [INT] PID racine
 function subtree
 {
-        while IFS=":" read enfant parent
-        do
-                if [ "$parent" == "$1" ] 
-                then
-                        for (( i=0; i<$currentlevel; i++ ))
-                        do
-                                echo -e -n "|      "
-                        done
-                        echo -e "└------$enfant"
-                        currentlevel=$((currentlevel+1))
-                        subtree $enfant
-                        currentlevel=$((currentlevel-1))
-                fi
-        done < /tmp/list.csv
-
+   while IFS=":" read enfant parent
+   do
+      if [ "$parent" == "$1" ] 
+      then
+         for (( i=0; i<$currentlevel; i++ ))
+         do
+            echo -e -n "|      "
+         done
+         echo -e "└------$enfant"
+	 currentlevel=$((currentlevel+1))
+	 subtree $enfant
+	 currentlevel=$((currentlevel-1))
+      fi
+   done < /tmp/info
 }
 
 subtree 1
