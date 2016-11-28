1887b75e4ff1882da5112913d41a520ae2766160
jerome-Aulagne <jerome.aulagne@diiage.org>
2016-11-28 09:22:32 +0100

    Update ListprocAULAGNE.sh
    80 caractères max. ajout update

--- Scripts-List-process/ListprocAULAGNE.sh
+++ Scripts-List-process/ListprocAULAGNE.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-#AIM : Récupération de la liste des processus sous forme d'arborescence, en ajoutant le lien de parenté Parent/Enfant et en spécifiant le l'ID de processus et l'utilisateur qui a lançé le processus
+#AIM : Récupération de la liste des processus sous forme d'arborescence, en 
+# ajoutant le lien de parenté Parent/Enfant et en spécifiant le l'ID de 
+# processus et l'utilisateur qui a lançé le processus
 #AUTHORS :   
-#   -Jérôme AULAGNE
+#   -AULAGNE Jérôme
 #PARAMS : 
 #MODIF :
 
-# Utilisation de la commande "pstree" qui permet d'afficher sous forme d'arborescence , la liste des processus en respectant le lien de parenté , tout en affichant via les options "u" : l'utilisateur ayant lançé le processus , ainsi que l'option "p" : l'id de processus ; l'utilisation de la commande  "less" permet de parcourir l'affichage du contenu ( visualiseur )
+# Utilisation de la commande "pstree" qui permet d'afficher sous forme 
+# d'arborescence , la liste des processus en respectant le lien de parenté
+# , tout en affichant via les options "u" : l'utilisateur ayant lançé le 
+# processus , ainsi que l'option "p" : l'id de processus ; l'utilisation de la 
+# commande  "less" permet de parcourir l'affichage du contenu ( visualiseur )
 
 pstree -up | less
 
