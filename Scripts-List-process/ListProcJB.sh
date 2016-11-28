65e776c4513a32a8e4139218b955b4d84109ac8b
jbernard21 <jeremy.bernard@diiage.org>
2016-11-28 09:03:18 +0100

    Update ListProcJB.sh

--- Scripts-List-process/ListProcJB.sh
+++ Scripts-List-process/ListProcJB.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#AIM : script qui affiche la liste des processus et leurs enfants
+#AUTHORS : BERNARD Jérémy
+#PARAMS :
+#   - aucun paramètre dans ce script
+#MODIF :
+#   - changement au niveau de la mise en forme du script
 
 pstree -aup
