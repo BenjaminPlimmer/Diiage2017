0463e975211001ccce5a0093476baa2f8a1b47b9
Saturn21 <nicolas@ngourillon.com>
2017-01-02 20:33:22 +0100

    Update process.sh
    Modification d'affichage de l'arbre + Ajout de fonctionnalité "kill processus"

--- GOURILLON/process.sh
+++ GOURILLON/process.sh
@@ -7,7 +7,8 @@
 #@PARAMS :
 #   - Aucun paramètres
 #@MODIF :
-#   - Changement de la mise en forme
+#12/2016 - Changement de la mise en forme
+#01/2017 - Modification affichage arbre
 
 # Pour chacun des élements dans /proc analyser chacun des nom de 0 à 9
 for i in $(ls /proc | grep '[0-9]$')
@@ -16,32 +17,44 @@ for i in $(ls /proc | grep '[0-9]$')
         pid=$(grep -w "Pid:" /proc/$i/status  2>/dev/null | awk -F " " '{print $2}')
         ppid=$(grep -w "PPid:" /proc/$i/status 2>/dev/null | awk -F " " '{print $2}')
         name=$(grep -w "Name:" /proc/$i/status 2>/dev/null| awk -F " " '{print $2}')
-        echo $pid';'$ppid >>/root/listproc
+        echo $i';'$ppid';'$name >>/tmp/listproc
 done
 
 
 currentlevel=0
-root='1'
+#root='1'
 function subtree
 {
-
         # Affiche un arbre à partir du noeud passé en argument
 
-        while IFS=";" read ppid pid
+        while IFS=";" read pid ppid name
         do
                 #echo "test $enfant ; $parent"
-                if [ "$pid" == "$1" ] # Cherche et affiche les enfants du noeud actuel
+                if [ "$ppid" == "$1" ] # Cherche et affiche les enfants du noeud actuel
                 then
                         for (( i=0; i<$currentlevel; i++ ))
                         do
                                 echo -e -n "|      "
                         done
-                        echo -e "└------$ppid"
+                        echo -e "└------$pid($name)"
                         currentlevel=$((currentlevel+1))
-                        subtree $ppid
+                        subtree $pid
                         currentlevel=$((currentlevel-1))
                 fi
-        done < listproc
+        done < /tmp/listproc
 }
-subtree 1
-rm /root/listproc
+subtree 0
+rm /tmp/listproc
+
+echo "Pour tuer un processus merci de rentrer son pid, pour quitter taper: 'quit' "
+read kill
+
+if [ "$kill" == "quit" ]
+then 
+        echo "Sortie du script"
+        exit
+else
+
+kill -9 $kill
+fi
+
