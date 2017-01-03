7ef17448c8eed4c6f68def70c614f786e5c32126
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2017-01-03 14:29:28 +0100

    Add minishell script

---
+++ PLIMMER/minishell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#@AIM : minishell , execute command and show return code
+#@AUTHORS : Benjamin PLIMMER
+#@PARAMS : none
+
+Exit=0
+while [[ $Exit != 1 ]]
+do
+	read -p "minishell : (press enter to leave) " $Command
+	if [[ -z "$Command" ]]
+	then
+		Exit=1
+	else
+		$Command
+		echo ${?}
+	fi
+
+done
+
+
