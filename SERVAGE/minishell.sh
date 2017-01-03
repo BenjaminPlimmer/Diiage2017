bbda5c05213cc68d1accfecbd8caeb6588786b96
thomasservage <thomas.servage@diiage.org>
2017-01-03 14:54:07 +0100

    test asynchrone

--- SERVAGE/minishell.sh
+++ SERVAGE/minishell.sh
@@ -1,11 +1,39 @@
-#!bin/bash
+#!/bin/bash
 #@AIM :Afficher un minishell
 #@AUTHORS :THOMAS SERVAGE
 #@PARAMS :
 #@MODIFS :
 read -p "saisir une commande :" Commande
-exec $Commande | echo ${?}
+
+exec $Commande  | echo ${?} &
+
+
+async1()
+{
+        sleep 5
+        echo " async 1"
+        sleep 5
+        echo "async 1 finished"
+        return 0
+}
+
+async2()
+{
+        sleep 3
+        echo "async 2"
+        sleep 4
+        echo "async 2 finished"
+        return 0
+}
+
+echo "En cours"
+async1 &
+async2 &
+
+
+
 exit 0
+
 
 
 
