8c61a07bdd103563cd3c039a4374be828fc278a3
jbernard21 <jeremy.bernard@diiage.org>
2017-01-03 08:51:29 +0100

    Upadate process.sh

--- BERNARD/process.sh
+++ BERNARD/process.sh
@@ -4,12 +4,45 @@
 #PARAMS: aucun paramètre
 #MODIF:
 
-#recherche des processus en utilisant une bouche for
-for P in $(seq 1 32618); do
-   [ -d "/proc/$P" ] || continue
-   echo "$P: $(cat /proc/$P/cmdline 2>/dev/null)"
-# recherche des PPID par processus
-   grep -E PPid /proc/$P/status
-# Mettre en place l'arborescence des processus en joignant les PPID avec les PID
-# Je n'ai pas trouvé la syntaxe pour le faire
+list=$(ls /proc/ | grep"[0-9]")
+
+#Parcours la liste des processus dans /proc
+#Stockage dans des variables du nom, du PPID et du PID
+
+for l in $list; do
+   name=$(more /proc/$l/status 2>/dev/null | grep "Name" | awk -F " " {'print $2'})
+   ppid=$(more /proc/$l/status 2>/dev/null | grep "PPid" | awk -F " " {'print $2'})
+   pid=$(more /proc/$l/status 2>/dev/null | grep "Pid" | awk -F " " {'print $2'})
 done
+
+#AIM: Fonction ayant pour but la mise en place d'une arborescence
+#     des processus et des processus enfants
+#     format du fichier : "pid:name:ppid"
+#PARAMS: int pid
+#RETURN:
+
+currentlevel=0
+
+#Affichage de l'arbre
+
+function Tree()
+{
+   while IFS=":" read enfant name parent
+   do
+      if [ "$parent" == "$1" ]; then
+         for (( i=0;i<$currentlevel;i++ )); do
+            echo -e -n "|     "
+         done
+         echo -e "L_____$enfant($name)"
+         currentlevel=$((currentlevel+1))
+         Tree $enfant
+         currentlevel=$((currentlevel-1))
+      fi
+   done < /tmp/process
+}
+
+Tree 0
+
+#Suppression du fichier
+
+rem /tmp/process
