fc1c6fbffb13df7810fdfb80df01e01e349def7e
PetitAlexandre <petit.alexandre.21@gmail.com>
2017-01-06 17:56:13 +0100

    Ajout du script du TP
    Script d'exécution d'une commande avec son code erreur qui est retourné.

--- PETIT/minishell.sh
+++ PETIT/minishell.sh
@@ -4,17 +4,31 @@
 #@PARAMS : None
 #@MODIF : None
 
+#Creation de la fonction cmd qui va recuperer la commande et son code erreur
+function cmd
+{
+        $1
+        res=$?
 
-#Lancement du shell
-while [ shell != "exit" ]
-do
-	#Traitement de la variable $shell
-        read -p "Taper une commande : " shell
-        $shell
-
-        #Condition sur le code erreur à afficher ou non
-        if [ $? != "0" ]
-                then
-                        echo "Code erreur $?"
+        if [ $res != "0" ]
+        then
+                echo "Code erreur $res"
         fi
-done
+}
+
+#Declaration de la variable shell pour le while
+shell="a"
+
+#while
+while [ "$shell" != "q" ]
+do
+
+		#Lecture de la saisie dans la variable shell et affichage du texte 
+        read -p "Taper une commande : " shell
+
+        #test pour verifier ce qui arrive en entree, si different de q on continue
+        if [ "$shell" != "q" ]
+        then
+                cmd "$shell" &
+
+        fi
