889e8f56adb64ba3238d7d0c7e68fb730862fea7
jerome-Aulagne <jerome.aulagne@diiage.org>
2017-01-23 10:00:59 +0100

    Update minishellAsync.sh

--- AULAGNE/minishellAsync.sh
+++ AULAGNE/minishellAsync.sh
@@ -6,13 +6,21 @@
 #03/01/2017 - Ajout du script de demande à l'utilisateur d'une commande et son éxécution en affichant son code erreur ( retour)'
 #05/01/2017 - Modification qui lance la commande de manière asynchrone et affiche son code erreur ( retour ).
 
-#AIM : Boucle qui va demander à l'utilisateur de taper une commande tant que "q" équivalent à quit n'est pas renseigné.
-#PARAMS : 
 
+#AIM : Boucle qui va demander à l'utilisateur de taper une commande tant que "q" équivalent à quit n'est pas renseigné.
+#PARAMS :
 
 while [[ $reponse != "q" ]] #tant que l'utilisateur ne tape pas "q" , on demande une commande a executer.
   do
     read -p "Entrez une commande :" reponse	#on recupere la commande
-    exec $reponse & > /dev/null #on execute la commande de manière asynchrone
-	echo ${?}  # on affiche le code erreur de la commande executée.	
-done
+	
+	if [[ $reponse != "q" ]]
+	then
+		exec $reponse & #on execute la commande de manière asynchrone
+		echo ${?}  # on affiche le code erreur de la commande executée.
+	else
+		exit
+	fi
+    	
+  done
+
