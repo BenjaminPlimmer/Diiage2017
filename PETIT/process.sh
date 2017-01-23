f4144d1332b91b5f9d305f5f2ff50d24da2df278
PetitAlexandre <petit.alexandre.21@gmail.com>
2017-01-23 01:50:04 +0100

    Modification du script
    Refonte du script.
    J'espère que la machine à café ne sera pas en panne.

--- PETIT/process.sh
+++ PETIT/process.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
-#@AIM: Script d affichage des processus parents et enfants v2
-#@AUTHOR: PETIT Alexandre
-#@PARAMS : None
-#@MODIF:
+#@AIM : Affichage des processus parents, enfants et kill du process
+#@AUTHORS :
+#   - Alexandre PETIT
+#@PARAMS :
+#@MODIF :
 
-#Liste des differents processus
-procs=$(ls /proc/ | grep "[0-9]")
+function tree
+{
+        # Affichage de l'arbre
+        while IFS=":" read id parent nom
+        do
+			#Condition sur le parent
+            if [ "$parent" == "$1" ]
+            then
+                for (( i=0; i<$currentLevel; i++ ))
+                do
+                    echo -e -n "|      "
+                    done
+                    echo -e "└------$id($nom)"
+                    currentLevel=$((currentLevel+1))
+                    tree $id
+                    currentLevel=$((currentLevel-1))
+                fi
+        done < /tmp/tmptree
+}
 
-for i in $procs; do
-
-        #Posistionnement dans le dossier concerne
-        cd /proc/$i
-
-        #Ecriture du processus
-        printf "Num Processus : $i"
-        #Saut de ligne
-        echo " "
-
-        # Affichage du Nom du processus
-        awk -F " " 'NR == 1, NR == 1 {print "Nom Processus : " $2}' status
-
-        #Affichage du PPID du processus en fonction du fichier status
-        awk -F " " 'NR == 5, NR == 5 {print "Num Processus Parent :  " $2}' status
-
-        #Separation
-        echo "____"
-
+# Listing des process contenant 0 à 9
+for i in $(ls /proc | grep '[0-9]$')
+do
+    #Stockage des données
+    id=$(grep -w "Pid:" /proc/$i/status  2>/dev/null | awk -F " " {'print $2'})
+    parent=$(grep -w "PPid:" /proc/$i/status 2>/dev/null | awk -F " " {'print $2'})
+    nom=$(grep -w "Name:" /proc/$i/status 2>/dev/null| awk -F " " {'print $2'})
+	
+	#Stockage des données dans le fichier tmptree
+    echo "$i:$parent:$nom" >> /tmp/tmptree	
 done
+
+currentLevel=0
+tree 0
+
+#Suppression du tmptree
+rm /tmp/tmptree
+
+#Kill du processus souhaité
+read -p "Saisir le processus à tuer, sinon <exit> : " id
+
+#Test si id égal exit
+if [ "$id" == "exit" ]
+then
+	#Le script s'arrête
+    exit
+else
+	#On kill le process
+	kill $id
+fi
