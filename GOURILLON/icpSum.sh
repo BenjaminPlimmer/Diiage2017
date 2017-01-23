79ac0e281a8be498a4a3fd3da9622de7f3599cbf
Saturn21 <nicolas@ngourillon.com>
2017-01-23 11:13:43 +0100

    Update icpSum.sh

--- GOURILLON/icpSum.sh
+++ GOURILLON/icpSum.sh
@@ -6,6 +6,6 @@
 #   - Aucun paramètres
 #@MODIF :
 #23/01/2017 - Création du script
-#Envoi de la commande ipcs dans l'entré de awk, puis somme de colone5
+#Envoi de la commande ipcs dans l'entré de awk, verification d'un entier avec une expression reguliére, puis somme de la colone5
 
 ipcs |awk '{ if ($5 ~ /^[0-9]*$/) SUM += $5} END { print "La somme mémoire des IPC est de " SUM/1024/1024 " Mo"}'
