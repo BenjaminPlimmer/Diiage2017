b9458ebff1b9bfe558cb798a951c906df604db07
thomasservage <thomas.servage@diiage.org>
2017-01-23 10:44:05 +0100

    change

--- SERVAGE/hids.sh
+++ SERVAGE/hids.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-Fichier="/var/log/auth.log"
-Temp=/tmp/auth.txt
+
+# Temp=/tmp/auth.txt
+
+if [[ ! -f "$1" ]]; then
+        echo -e "Le fichier $1 n'existe pas."
+        exit
+fi
+
+Fichier=$1
 while [ true ]
 do
-        inotifywait -e modify $Fichier
+    inotifywait -e modify $Fichier
+	echo " $Fichier a été modifié" 
+	exit	
+        
+done
+while [ true ]
+do
+        inotifywait -e modify /var/log/auth.log
 
         if tail -n1 /var/log/auth.log  |grep "authentication failure"
         then
                         echo -e "Attention erreur d'authentification magueule"
         fi
 done
+
+
 
 
