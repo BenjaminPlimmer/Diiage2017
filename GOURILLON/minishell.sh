f6289f6be804241c3c540d1c86aa8b07f95430ae
Saturn21 <nicolas@ngourillon.com>
2017-01-23 09:46:31 +0100

    Update minishell.sh

--- GOURILLON/minishell.sh
+++ GOURILLON/minishell.sh
@@ -5,10 +5,15 @@
 #@PARAMS :
 #   - Aucun paramètres
 #@MODIF :
-#
+#23/01/2017 - Ajout de la boucle while
 
-echo "Saisir le processus :"
-read name
-$name
-
-echo "Le code retour de $name est ${?}"
+name=""
+while [[ $name != "exit" ]]
+do
+        echo "Saisir le processus :"
+        read name
+        #Affichage du resultat de la commande
+        $name
+        #Affichage du code retour
+        echo "Le code retour de $name est ${?}"
+done
