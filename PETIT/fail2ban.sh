912ca62480eb81585b488f49429acb1eaf5021e0
PetitAlexandre <petit.alexandre.21@gmail.com>
2017-01-26 14:25:54 +0100

    Ajout de la partie inotifywait

--- PETIT/fail2ban.sh
+++ PETIT/fail2ban.sh
@@ -5,15 +5,17 @@
 #@PARAMS : None
 #@MODIF : None
 
-#Suppression du fichier avant le lancement
-rm /tmp/tmpip
-#Récupération des dernières ip du auth.log qui contiennent "Failed password"
-tail /var/log/auth.log | grep "Failed password" |awk {'print $11'} >> /tmp/tmpip
 
+#Lancement de inotifywait
+while inotifywait -e modify /var/log/auth.log
+do
+	#Récupération des dernières ip du auth.log qui contiennent "Failed password"
+	tail /var/log/auth.log | grep "Failed password" |awk {'print $11'} >> /tmp/tmpip
 #Parcours du fichier tmpip pour bannir les ip récupérées
-for i in $(cat /tmp/tmpip); 
-do	
-	iptables -I INPUT -s $i -j DROP
-	echo "L'adresse suivante a été bannie : " $i
+	for i in $(cat /tmp/tmpip); 
+	do	
+		iptables -I INPUT -s $i -j DROP
+		echo "L'adresse suivante a été bannie : " $i
+		rm /tmp/tmpip
+	done
 done
-
