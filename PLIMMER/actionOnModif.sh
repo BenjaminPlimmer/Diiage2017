e07e52193ff61e600288e92453df772d820bb67e
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2017-01-26 16:54:30 +0100

    actionOnModif finish exept regex
    regex in actionOnModif.conf not working
    No check parameters or files

---
+++ PLIMMER/actionOnModif.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#@AIM : Fail2Ban , script Bash implémentant un système de détection d'intrusion 
+#@AIM : activité suspecte sur un serveur GNU/Linux via la surveillance "temps réel" des logs d'activité.
+#@AUTHORS : Benjamin PLIMMER
+#@PARAMS : none
+
+#AIM : get last line of file
+#PARAMS : [string] file
+function getLastLine
+{
+    last=$(tail -1 $1)
+}
+
+#AIM : check regex
+#PARAMS : [string] regex , [string] line
+function matchRegex
+{
+[[ $2 =~ $1 ]] && ((nb++))
+matched="${BASH_REMATCH[1]}"
+}
+
+
+#Start script
+
+nb=0
+while read LINE
+do 
+	file=$(awk '$1 ~ /file=+/ {print $1}' actionOnModif.conf)
+	regex=$(grep "regex=" actionOnModif.conf)
+	action=$(grep "action=" actionOnModif.conf)
+	count=$(awk '$1 ~ /count=+/ {print $1}' actionOnModif.conf)
+done < actionOnModi.conf
+
+
+file=$(echo "${file/file=/}")
+regex=$(echo "${regex/regex=/}")
+action=$(echo "${action/action=/}")
+count=$(echo "${count/count=/}")
+
+
+while inotifywait -e modify "$file"
+do
+	getLastLine $file
+
+	matchRegex $regex $last
+	if [ "$nb" == "$count" ]
+	then
+		command=$(echo "${action/match/"$matched"}")
+		$command
+		echo "executed: $command"
+		nb=0
+	fi
+	
+	echo "$nb"
+	echo "$matched"
+
+done
