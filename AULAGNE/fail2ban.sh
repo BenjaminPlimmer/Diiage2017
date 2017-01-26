91a8d0328fc68d85403c296a1b8d759e91c0d557
jerome-Aulagne <jerome.aulagne@diiage.org>
2017-01-26 15:50:20 +0100

    Update fail2ban.sh

--- AULAGNE/fail2ban.sh
+++ AULAGNE/fail2ban.sh
@@ -4,21 +4,24 @@
 #PARAMS : 
 #MODIF : 
 #25-01-17 - ajout du script fail2ban.
+#26-01-17 - Optimisation ( test tous les modifs sur les fichiers de log.
 
+#on stocke le chemin du fichier de log.
+#Log='/var/log/auth.log'
 
 #on stocke la date dans une variable.
 date="date"
 
 #on stocke le nombre limite de tentatives de mauvais mot de passe.
-badCount="2"
+badCount="3"
 
-#AIM : Boucle sur les event tant que le fichier de log auth est modifié.
+
+#AIM : Fonction ban ip si mot de passe érroné .
 #PARAMS :
-inotifywait -m -e modify /var/log/auth.log | while read event file
-do
-	echo "Fichier modifié"
+function banSshFailed
+{
 	#Si on trouve une ligne avec le mot failed  et ssh on envoi le nombre de tentatives et l'adresse ip dans le fichier temporaire IP-file.
-   if (awk '/Failed/ && /ssh/ {x[$(NF-3)]++} END {for (i in x){printf "%1d %s\n", x[i], i}}' /var/log/auth.log| sort -nr ) > /tmp/IP-file;
+   if (awk '/Failed/ && /ssh/ {x[$(NF-3)]++} END {for (i in x){printf "%1d %s\n", x[i], i}}' $Log| sort -nr ) > /tmp/IP-file;
    then
 		#on met le nombre de tentatives dans une variable.
 		count=$(cat /tmp/IP-file | cut -d" " -f1)
@@ -29,10 +32,45 @@ do
 		#Si le nombre de tentatives dépasse 2 on bannit l'adresse IP ( DROP )
 		if  [ $count -ge $badCount ]
 		then
-				# On indique dans /var/log/messages que l'on drop une IP & on affiche le ban avec l'adresse ip dans le terminal.
+				# On indique dans /var/log/messages que l'on drop une IP
 				echo $($date)." Ban IP : "$ip
 				echo $($date)." Drop de l'IP : "$ip >> /var/log/messages;
 				/sbin/iptables -I INPUT -s $ip -j DROP;
 		fi	
+		
+		#on supprime le fichier temporaire.
+		rm /tmp/IP-file
    fi
+}
+
+#AIM : Boucle sur les event tant que le fichier de log est modifié.
+#PARAMS :
+
+while [ -f $Log ]
+read -p "Entrez un fichier de log (si contrôle accès ssh => /var/log/auth.log) :" Log
+do
+
+	if [ -f $Log ]
+	then
+	
+		inotifywait -m -e modify $Log | while read event file
+		do
+			#on affiche que le fichier a été modifié.
+			echo "Fichier modifié : " $Log
+	
+			#Si le fichier de log est auth.log , on appelle la fonction banSshFailed pour ban l'ip sinon on affiche la dernière ligne du fichier
+			if [[ $Log = '/var/log/auth.log' ]]
+			then
+				echo "Tentative d'attaque par ssh"
+				banSshFailed
+			else
+				awk '{ ligne=$0 } END { print ligne }' $Log
+			fi
+	
+	
+		done
+	else
+		echo 'Ce fichier de log n/existe pas'
+	
+	fi
 done
