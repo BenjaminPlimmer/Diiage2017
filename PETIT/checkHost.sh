a728475f83fe43d05d2221ea4165b45f9ed98e89
PetitAlexandre <petit.alexandre.21@gmail.com>
2017-01-30 09:11:28 +0100

    Modification script checkHost
    Ajout du listing et kill process

--- PETIT/checkHost.sh
+++ PETIT/checkHost.sh
@@ -8,6 +8,7 @@
 ######################################################
 #Déclaration des fonctions
 ######################################################
+#Variables pour monitoring
 function Stockage 
 {
 	hostnam=$(hostname)
@@ -19,6 +20,61 @@ function Stockage
 	proc=$(ps -o %cpu |awk 'NR==2')
 }
 
+#Process
+function tree
+{
+        # Affichage de l'arbre
+        while IFS=":" read id parent nom
+        do
+	    #Condition sur le parent
+            if [ "$parent" == "$1" ]
+            then
+                for (( i=0; i<$currentLevel; i++ ))
+                do
+                    echo -e -n "|      "
+                    done
+                    echo -e "└------$id($nom)"
+                    currentLevel=$((currentLevel+1))
+                    tree $id
+                    currentLevel=$((currentLevel-1))
+                fi
+        done < /tmp/tmptree
+}
+
+function killProcess
+{
+	# Listing des process contenant 0 à 9
+for i in $(ls /proc | grep '[0-9]$')
+do
+    #Stockage des données
+    id=$(grep -w "Pid:" /proc/$i/status  2>/dev/null | awk -F " " {'print $2'})
+    parent=$(grep -w "PPid:" /proc/$i/status 2>/dev/null | awk -F " " {'print $2'})
+    nom=$(grep -w "Name:" /proc/$i/status 2>/dev/null| awk -F " " {'print $2'})
+	
+    #Stockage des données dans le fichier tmptree
+    echo "$i:$parent:$nom" >> /tmp/tmptree	
+done
+
+currentLevel=0
+tree 0
+
+#Suppression du tmptree
+rm /tmp/tmptree
+
+#Kill du processus souhaité
+read -p "Saisir le processus à tuer, sinon <exit> : " id
+
+#Test si id égal exit
+if [ "$id" == "exit" ]
+then
+    #Le script s'arrête
+    Menu    
+else
+    #On kill le process
+    kill $id
+    Menu
+fi
+}
 
 #Affichage des données
 function Affichage
@@ -45,7 +101,7 @@ function Affichage
 #Affichage du menu
 function Menu 
 {
-	read -p "Que faire ? <a>: Actualiser ; <q>: Quitter : " selec
+	read -p "Que faire ? <a>: Actualiser ; <p> : Liste & Kill Process ; <q>: Quitter : " selec
 
 	case "$selec" in
 		a)  Attente
@@ -54,6 +110,9 @@ function Menu
 			Menu
 		;;
 
+		p)  killProcess
+		;;
+		
 		q)  exit
 		;; 
 
