e76c1c3037d4d9b36ecb64d33662c21a88060676
zukias39 <amadieu.r@gmail.com>
2017-01-30 11:47:20 +0100

    add calcul putyon

---
+++ AMADIEU/calcul.py
@@ -0,0 +1,14 @@
+def add(nb1,nb2):
+	res=nb1+nb2
+	print (res)
+
+def multiplication(nb1):
+	for x in range (1,10):
+		nb2=nb1*x
+		print (nb2)
+
+print ("Additionne 1 + 2")
+add(1,2)
+
+print ("Tableau multiplacation de 5")
+multiplication(5)
