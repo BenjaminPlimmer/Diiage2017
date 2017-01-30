3cab7791e39712349f26750170f1e0a113f11ad0
BenjaminPlimmer <plimmer.benjamin@gmail.com>
2017-01-30 11:47:02 +0100

    add calcul.py

---
+++ PLIMMER/calcul.py
@@ -0,0 +1,13 @@
+def add (p1,p2):
+	print(p1+p2)
+	return True
+
+def multiply (p1):
+	for i in range(1, 11):
+		print(p1 ,"X", i, "=", p1*i)
+		
+	return True
+
+multiply(5)
+
+add(8554475,656589678)	
