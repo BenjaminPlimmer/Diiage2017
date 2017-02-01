eedb660573cf06a32eeb84bf78813f1e84d50d80
Saturn21 <nicolas@ngourillon.com>
2017-02-01 14:57:53 +0100

    Update sys.sh

--- GOURILLON/sys.sh
+++ GOURILLON/sys.sh
@@ -7,37 +7,38 @@
 #@MODIF :
 #01/02/2017-PARTIEL-Création du script
 #
+echo "------------------------------------"
+echo "Informations système de $HOSTNAME "
+echo "------------------------------------"
 
-echo "Liste des disques montés:"
+echo "1 - Liste des disques montés:"
 lsblk
 echo "-----------------------------"
 
+echo "2 - Espace disque total disponible"
+df -k | awk '{ SUM += $3} END { print SUM/1024/1024 "G"}'
+echo "-----------------------------"
 
-echo "Nombre de processus en cours:"
+echo "3 - Nombre de processus en cours:"
 ps -ef | wc -l
 echo "-----------------------------"
 
 
-
-
-echo "Liste des IPC actuels:"
+echo "4 - Liste des IPC actuels:"
 ipcs
 echo "-----------------------------"
 
-
-
-echo "Le processus le plus gourmand en CPU"
+echo "5 - Le processus le plus gourmand en CPU"
 ps aux | sort -nrk 3,3 | head -n 1
 echo "-----------------------------"
 
 
-
-echo "L'uptime du système est de:"
+echo "6 - L'uptime du système est de:"
 uptime -p
 
 echo "-----------------------------"
 
 
-echo "Taux de remplisage des tables d'inodes"
+echo "7 - Taux de remplisage des tables d'inodes"
 df -h | awk '{if ($1 != "Filesystem") print $1 " " $5}'
 echo "-----------------------------"
