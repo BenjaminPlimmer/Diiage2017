daa3315f544b6dfbe9c09e4a70e644b69e1ad817
KBaruet <kevin.baruet@outlook.fr>
2017-02-01 14:57:40 +0100

    Commit Partiel

---
+++ BARUET/menu.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# AIM     : Retrouver un utilisateur, son UID, q pour quitter
+# AUTHORS : KEVIN BARUET
+# PARAMS  : Pas de param
+# MODIFS  : 
+
+function menu
+{
+   echo " "
+   echo " ---------------------------------------------- "
+   echo "|                    Menu                      |"
+   echo " ---------------------------------------------- "
+   echo "|  Entrer une valeur :                         |"
+   echo "|                                              |"
+   echo "| 1 : Verifier l'existence d'un utilisateur    |"
+   echo "| 2 : Retrouver l'UID de l'utilisateur         |"
+   echo "| q : quitter                                  |"
+   echo " ---------------------------------------------- "
+   echo " "
+}
+
+function exist
+{
+   echo " "
+   echo " ------------------------------------------------------------------------- "
+   echo "| Donner un nom d'utilisateur - Entrez 'q' pour revenir au menu precedent |"
+   echo " ------------------------------------------------------------------------- "
+   echo " "
+   user="none"
+   while [ "$user" != "q" ]
+      do
+         echo "Donner un nom d'utilisateur : "
+         read user
+         if grep -w "$user" /etc/passwd > /bin/nul
+            then
+               echo " "
+               echo "$user existe bien."
+               echo " "
+            else
+               echo " "
+               echo "$user introuvable."
+               echo " "
+         fi
+      done
+}
+
+function UID
+{
+   echo " "
+   echo " ------------------------------------------------------------------------- "
+   echo "| Donner un nom d'utilisateur - Entrez 'q' pour revenir au menu precedent |"
+   echo " ------------------------------------------------------------------------- "
+   echo " "
+   user="none"
+   while [ "$user" != "q" ]
+      do
+         echo "Donner un nom d'utilisateur : "
+         read user
+         if  grep -w "$user" /etc/passwd > /bin/nul
+            then
+               echo " "
+               echo "l'UID de $user est : "
+               grep -w "$user" /etc/passwd > /tmp/menutmp
+               awk  -F ":" '{print $3}' /tmp/menutmp
+               rm /tmp/menutmp
+               echo " "
+            else
+               echo " "
+               echo "$user introuvable."
+               echo " "
+         fi
+      done
+}
+
+val="0"
+while [ "$val" != "q" ]
+   do
+      menu
+      read val
+
+      case $val  in
+         "1") exist ;;
+         "2") UID ;;
+         "q") exit ;;
+         *) echo "$val n'est pas une valeur comprise dans le menu"
+            echo "Veuillez recommencer.";;
+      esac
+   done 
+
---
+++ BARUET/sys.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# AIM     : Utiliser les differents scripts crees jusqu'ici pour en faire un global qui nous donne une indication
+#	    sur l'etat du serveur.
+# AUTHORS : Kevin BARUET
+# PARAMS  : Pas de parametre.
+# MODIFS  :
+
+function Ipcs
+{
+   echo " "
+   echo "IPCs ouverts :"
+   echo "------------------------ "
+   ipcs -m
+   echo "------------------------ "
+}
+
+function DiskMount
+{
+   echo " "
+   echo "Disques montes :"
+   echo "------------------------ "
+   df | awk '{print $1 " monte sur " $6}'
+   echo "------------------------ "
+}
+
+function FreeSpace
+{
+   echo " "
+   echo "Espace libre :"
+   echo "------------------------ "
+   free -h | awk 'NR==2 {print $4}'
+   echo "------------------------ "
+}
+
+function ProcSum
+{
+   echo " "
+   echo "Nombre de processus en cours :"
+   echo "------------------------ "
+   ps -aux | awk '{procsum+=1} END {print procsum}'
+   echo "------------------------ "
+}
+
+function ProcCPU
+{
+   echo " "
+   echo "Processus le plus gourmand :"
+   echo "------------------------ "
+   ps -aux --sort=-pcpu | head -n 2
+   echo "------------------------ "
+}
+
+function Uptime
+{
+   echo " "
+   echo "Uptime :"
+   echo "------------------------ "
+   uptime | awk '{print $3}'
+   echo "------------------------ "
+}
+
+function Inodes
+{
+   echo " "
+   echo "Taux de remplissage des tables d'inodes :"
+   echo "------------------------ "
+   df -ih | awk '{print $1 " contient " $2 " inodes"}'
+   echo "------------------------ "
+}
+
+function Menu
+{
+   echo " "
+   echo " ---------------------------------------------- "
+   echo "|            Etat global du serveur            |"
+   echo " ---------------------------------------------- "
+   echo "|  Entrer une valeur :                         |"
+   echo "|                                              |"
+   echo "| 1 : Disques montes                           |"
+   echo "| 2 : Espace total disponible                  |"
+   echo "| 3 : Nombre de processus en cours             |"
+   echo "| 4 : IPCs ouvert sur la machine               |"
+   echo "| 5 : Le processus le plus gourmand en CPU     |"
+   echo "| 6 : Uptime                                   |"
+   echo "| 7 : Taux de remplissage des tables d'inodes  |"
+   echo "|                                              |"
+   echo "| M : Affichage du Menu                        |"
+   echo "|                                              |"
+   echo "| Entrer 'Stop' pour terminer le programme     |"
+   echo " ---------------------------------------------- "
+   echo " "
+}
+
+Menu
+val="1"
+while [ "$val" != "stop" ]
+   do
+      echo " "
+      echo "Donner une valeur :" 
+      read val
+      echo " "
+
+      case $val  in
+         "1")  DiskMount;;
+         "2")  FreeSpace;;
+         "3")  ProcSum;;
+         "4")  Ipcs  ;;
+         "5")  ProcCPU;;
+         "6")  Uptime;;
+         "7")  Inodes;;
+         "M") Menu ;;
+         "stop") exit ;;
+         *) echo "$val n'est pas une valeur comprise dans le menu"
+         echo "Veuillez recommencer.";;
+      esac
+   done 
