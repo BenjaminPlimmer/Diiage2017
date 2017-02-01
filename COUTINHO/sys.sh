91a002aec1fac44acc07a8f23e1d165c460e08f5
NCoutinhoDotFr <contact@ncoutinho.fr>
2017-02-01 15:29:57 +0100

    Add files via upload

--- COUTINHO/sys.sh
+++ COUTINHO/sys.sh
@@ -6,12 +6,12 @@
 function afficheMenu ()
 {
         echo -e "\n1 - Disques montés\n"
-        echo -e "2 - Espace disque total disponible\n"
-        echo -e "3 - Nombre de processus en cours\n"
-        echo -e "4 - IPC ouverts sur la machine\n"
-        echo -e "5 - Processus le plus groumand en CPU\n"
-        echo -e "6 - Uptime\n"
-        #echo -e "7 - Taux remplissage des tables d'Inodes\n"
+	echo -e "2 - Espace disque total disponible\n"
+	echo -e "3 - Nombre de processus en cours\n"
+	echo -e "4 - IPC ouverts sur la machine\n"
+	echo -e "5 - Processus le plus groumand en CPU\n"
+	echo -e "6 - Uptime\n"
+	#echo -e "7 - Taux remplissage des tables d'Inodes\n"
 }
 
 #AIM : Fonction permettant d'afficher les disques montés
@@ -19,7 +19,7 @@ function afficheMenu ()
 #RETURN : None
 function diskMountAndSpace()
 {
-        df -h
+	df -h
 }
 
 #AIM : Fonction permettant d'afficher les ipcs
@@ -51,23 +51,22 @@ function processCPU()
 #RETURN : None
 function nbProc()
 {
-        echo -e "$(ps -u "$(echo $(w -h | cut -d ' ' -f1 | sort -u))" o user= | sort | uniq -c |$
+	echo -e "$(ps -u "$(echo $(w -h | cut -d ' ' -f1 | sort -u))" o user= | sort | uniq -c | sort -rn | cut -d ' ' -f5) processus actifs"
 }
-
 
 while [ "$input" != "q" ]
 do
-        afficheMenu
-        read -p "Action: " input
-        case $input  in
-                "1") diskMountAndSpace ;;
-                "2") diskMountAndSpace ;;
-                "3") nbProc ;;
-                "4") ipc ;;
-                "5") processCPU ;;
-                "6") upTime ;;
-                #"7") inodes ;;
-                "q") exit ;;
-                *) echo -e "\t$input n'est pas une valeur comprise" ;;
-        esac
+	afficheMenu
+	read -p "Action: " input
+	case $input  in
+		"1") diskMountAndSpace ;;
+		"2") diskMountAndSpace ;;
+		"3") nbProc ;;
+		"4") ipc ;;
+		"5") processCPU ;;
+		"6") upTime ;;
+		#"7") inodes ;;
+		"q") exit ;;
+		*) echo -e "\t$input n'est pas une valeur comprise" ;;
+	esac
 done
